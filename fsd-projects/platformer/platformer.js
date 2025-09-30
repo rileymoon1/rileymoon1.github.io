ba1e64ae028f940e112e0300bf6a09d0910a5993
rileymoon1 <riley.moon@inspire35.org>
2025-09-30 19:18:06 +0000

    qwert

--- fsd-projects/platformer/platformer.js
+++ fsd-projects/platformer/platformer.js
@@ -17,7 +17,7 @@ $(function () {
     }
 
     // Create walls - do not delete or modify this code
-    createPlatform(-50, -50, canvas.width + 100, 50); // top wall
+    createPlatform(-50,-50, canvas.width + 100, 50); // top wall
     createPlatform(-50, canvas.height - 10, canvas.width + 100, 200, "navy"); // bottom wall
     createPlatform(-50, -50, 50, canvas.height + 500); // left wall
     createPlatform(canvas.width, -50, 50, canvas.height + 100); // right wall
@@ -27,23 +27,24 @@ $(function () {
     //////////////////////////////////
 
     // TODO 1 - Enable the Grid
-    // toggleGrid();
+    toggleGrid();
 
 
     // TODO 2 - Create Platforms
-
-
-
+createPlatform(250, 552, 200, 100);
+createPlatform(350, 150, 200, 100);
+createPlatform(700, 401, 200, 100);
+createPlatform(1100, 250, 200, 100);
 
     // TODO 3 - Create Collectables
-
-
-
+createCollectable("database", 300, 700);
+createCollectable("datebase", 250,551);
+createCollectable("database", 455, 110);
     
     // TODO 4 - Create Cannons
-
-
-    
+createCannon("left", 0, 550);
+createCannon("bottom", 551, 750);
+createCannon("top", 1050, 0);   
     
     //////////////////////////////////
     // ONLY CHANGE ABOVE THIS POINT //
